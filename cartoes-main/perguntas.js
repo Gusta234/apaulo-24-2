85d9b3266754bbb7c5a9bbbc14fb826eed667a1b
Gusta234 <165030753+Gusta234@users.noreply.github.com>
2024-10-22 09:53:29 -0300

    Add files via upload

---
+++ cartoes-main/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'pergunta 1',
+    'quantos times tem o brasileirao?',
+    'o brasileirao contem 20 times'
+)
+
+criaCartao(
+    'pergunta 2',
+    'Qunatos times sao rebaixados?',
+    '4 times sao rebaixados'
+)
+
+criaCartao(
+    'pergunta 3',
+    'quantos times vao para libertadores?',
+    '4 times vao direto e 2 sao qualificados'
+)
+
+criaCartao(
+    'pergunta 4',
+    'quantos times vao para sul americna?',
+    '6 times'
+)
+
+criaCartao(
+    'pergunta 5',
+    'quem foi vice da libertadores de 2022?',
+    'hatletico pr'
+)
+
+criaCartao(
+    'pergunta 6',
+    'quantas vezes o atletico pr foi rebaixado?',
+    '4 vezes'
+)
